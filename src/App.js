92590edb8882e3dc86acbf8e7425821e79a1458e
Marvellous1 <51808658+Marvellous1@users.noreply.github.com>
2020-09-17 01:00:13 +0100

    Fixed dispatcher Error

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import ClientProfile from "./Pages/client/ClientProfile";
 import ClientPackages from "./Pages/client/ClientPackages/ClientPackages";
-import ClientDashboard from "./pages/client/ClientDashboard/ClientDashboard"
+import ClientDashboard from "./Pages/client/ClientDashboard/ClientDashboard"
 
 import Navbar from "./Components/navbar/Navbar";
 import Slider from "./Components/slider/Slider";
-import Dispatcher from "./Pages/dispatcher/Dispatcher";
+import Dispatcher from "./pages/dispatcher/Dispatcher";
 
 function App() {
   return (
